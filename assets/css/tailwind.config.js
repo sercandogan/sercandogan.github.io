3f822fa888c79e990af7f8787c070d19bcdeabca
Sercan Dogan <sercandoganweb@gmail.com>
2024-08-01 00:02:57 +0200

    wip

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    darkMode: "media",
+    content: [
+      "./node_modules/flowbite/**/*.js",
+      "./layouts/**/*.html",
+      "./content/**/*.html"
+    ],
+    theme: {
+      extend: {}
+    },
+    plugins: [
+      require("flowbite/plugin"),
+    ]
+  }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sercandogan/sercandogan.github.io
+
+go 1.22.5
+
+require github.com/jpanther/congo/v2 v2.8.2 // indirect
